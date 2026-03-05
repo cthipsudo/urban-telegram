e986a35d069e0cc10589939166de52bb748ef955
GrouchyDodaoOT <chanpasong.thipphakhinkeo@gmail.com>
2026-03-05 18:38:40 -0500

    created a slot machine

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SlotsMachine from "./Slotsmachine";
+function App() {
+  return (
+    <>
+      <SlotsMachine slot1="🙈" slot2="🙉" slot3="🙈" />
+    </>
+  );
+}
+
+// 🙊
+
+export default App;
---
+++ src/Slotsmachine.jsx
@@ -0,0 +1,18 @@
+function SlotsMachine({ slot1, slot2, slot3 }) {
+  const winStatus = slot1 === slot2 && slot1 === slot3 && slot2 === slot3;
+  return (
+    <main>
+      <h1 style={{ display: "inline-block" }}>{slot1}</h1>
+      <h1 style={{ display: "inline-block" }}>|</h1>
+      <h1 style={{ display: "inline-block" }}>{slot2}</h1>
+      <h1 style={{ display: "inline-block" }}>|</h1>
+      <h1 style={{ display: "inline-block" }}>{slot3}</h1>
+      <h2 style={{ color: winStatus ? "green" : "red" }}>
+        {winStatus ? "You win!!" : "You Lose..."}
+      </h2>
+      {winStatus ? <h3>Congrats!!!!</h3> : ""}
+    </main>
+  );
+}
+
+export default SlotsMachine;
